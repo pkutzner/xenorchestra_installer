4157c9cde45211cb52158fa6a51313c757512df2
Jarli01 <dustinbeuerlein1@gmail.com>
2018-02-07 09:46:18 -0500

    Update xo_install.sh

--- xo_install.sh
+++ xo_install.sh
@@ -2,14 +2,13 @@
 
 xo_branch="master"
 xo_server="https://github.com/vatesfr/xen-orchestra"
-xo_web="https://github.com/vatesfr/xo-web"
 n_repo="https://raw.githubusercontent.com/visionmedia/n/master/bin/n"
 yarn_repo="deb https://dl.yarnpkg.com/debian/ stable main"
 node_source="https://deb.nodesource.com/setup_9.x"
 yarn_gpg="https://dl.yarnpkg.com/debian/pubkey.gpg"
 n_location="/usr/local/bin/n"
 xo_server_dir="/opt/xen-orchestra"
-xo_web_dir="/opt/xo-web"
+xo_web_dir="/opt/xo-xen-orchestra"
 systemd_service_dir="/lib/systemd/system"
 xo_service="xo-server.service"
 
@@ -30,18 +29,17 @@ sudo /usr/local/bin/n stable
 #Install XO dependencies
 sudo /usr/bin/apt-get install --yes build-essential redis-server libpng-dev git python-minimal libvhdi-utils nfs-common
 
-/usr/bin/git clone -b $xo_branch $xo_server
-/usr/bin/git clone -b $xo_branch $xo_web
+/usr/bin/git/clone -b $xo_server
 
 # Patch to allow config restore
-sed -i 's/< 5/> 0/g' /opt/xo-web/src/xo-app/settings/config/index.js
+sed -i 's/< 5/> 0/g' ../xo-web/dist/src/xo-app/settings/config/index.js
 
 cd $xo_server_dir
 /usr/bin/yarn 
 /usr/bin/yarn build
 cd packages/xo-server
 sudo cp sample.config.yaml .xo-server.yaml
-sudo sed -i "s|#'/': '/path/to/xo-web/dist/'|'/': '/opt/xo-web/dist'|" .xo-server.yaml
+sudo sed -i "s|#'/': '/path/to/xo-web/dist/'|'/': '/opt/xen-orchestra/packages/xo-web/dist'|" .xo-server.yaml
 cd $xo_web_dir
 
 /usr/bin/yarn 
