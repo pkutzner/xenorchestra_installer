af596a2f583184b6fa01cc6eb2619d3cce113031
Jarli01 <dustinbeuerlein1@gmail.com>
2018-02-07 10:33:04 -0500

    Update xo_install.sh
    typo fixes.

--- xo_install.sh
+++ xo_install.sh
@@ -28,7 +28,7 @@ sudo /usr/local/bin/n stable
 #Install XO dependencies
 sudo /usr/bin/apt-get install --yes build-essential redis-server libpng-dev git python-minimal libvhdi-utils nfs-common
 
-/usr/bin/git/clone -b $xo_server
+/usr/bin/git clone -b $xo_server_dir
 
 # Patch to allow config restore
 sed -i 's/< 5/> 0/g' /opt/xen-orchestra/xo-web/dist/src/xo-app/settings/config/index.js
